65820a3432561d565416a116dfe52492bfddb96f
Tanner Elliott <Lynnte@byui.edu>
2023-09-26 21:33:04 -0600

    hamb

---
+++ scripts/hamburger.js
@@ -0,0 +1,27 @@
+// hamburger.js
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.getElementById('nav-toggle');
+    const mobileNav = document.querySelector('.mobile-nav');
+  
+    // Function to toggle the menu icon
+    function toggleMenuIcon() {
+      if (navToggle.checked) {
+        navToggle.nextElementSibling.innerHTML = '&#10006;'; // Close symbol
+      } else {
+        navToggle.nextElementSibling.innerHTML = '&#9776;'; // Hamburger menu
+      }
+    }
+  
+    // Close the mobile menu when a link is clicked
+    const mobileNavLinks = document.querySelectorAll('.mobile-nav a');
+    mobileNavLinks.forEach((link) => {
+      link.addEventListener('click', () => {
+        navToggle.checked = false;
+        toggleMenuIcon();
+      });
+    });
+  
+    // Initial call to set the menu icon
+    toggleMenuIcon();
+  });
+  
